6aa479e6b844c3c9730bf2a9223f1fa796fe5c4a
Abrish-seng <abrish814@gmail.com>
2024-11-14 19:27:17 +0300

    adding the other tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,38 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie using the ALX Star Wars API
 
-// a script that prints all characters of a Star Wars movie
-
+if (process.argv.length !== 3) {
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
-const movies = process.argv.slice(2);
 
-request.get(`https://swapi-api.hbtn.io/api/films/${movies}/`, async (_error, response, body) => {
-  for (const character of JSON.parse(body).characters) {
-    const people = await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(JSON.parse(body).name);
-        }
-      });
+// Recursive function to get and print character names from a list of URLs
+function printNextCharacter (urls) {
+  // Remove first URL from list and parse it
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
+      if (!error) {
+        // Parse and print the character name from the response body
+        console.log(JSON.parse(body).name);
+        // Move to next character in the list
+        printNextCharacter(urls);
+      }
     });
-    console.log(people);
   }
-});
+}
+
+// Fetches a list of character's URLs then uses a callback to print each one's name
+function printStarWarsCharacters (url) {
+  // Get list of URLs for characters in selected film
+  request(url, function (error, response, body) {
+    if (!error) {
+      // Call recursive function to fetch and print character names from URLs list
+      printNextCharacter(JSON.parse(body).characters);
+    }
+  });
+}
+
+printStarWarsCharacters(url);
